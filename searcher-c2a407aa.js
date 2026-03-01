33a3716152856c0e1d6ef06c3a5a779cf97f6772
wang-q <wang-q@users.noreply.github.com>
2026-03-01 21:41:37 +0000

    deploy: 79180a080e7024b4b6c8bcb9a899e42ee6132f3d

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-13a8f441.js',
+                path_to_root + 'searchindex-4af1891c.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
